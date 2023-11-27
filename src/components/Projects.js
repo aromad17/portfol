057300a1c45bd706ab034c023aaa5245a3f5e3bb
aromad17 <aromad1117@gmail.com>
2023-11-28 01:11:07 +0900

    새 프로젝트 추가

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,102 +1,112 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { FaHome, FaGithub } from "react-icons/fa";
 
-function Projects({ setIsClicked, setWorkName, keyNum, name, className, skill, function1, homepage, github, videoUrl, iosVideoUrl }) {
+function Projects({
+  setIsClicked,
+  setWorkName,
+  keyNum,
+  name,
+  className,
+  skill,
+  function1,
+  homepage,
+  github,
+  videoUrl,
+  iosVideoUrl,
+}) {
+  const mockupRef = useRef(null);
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-    const mockupRef = useRef(null);
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  useEffect(() => {
+    mockupRef.current = document.querySelectorAll(".mockup");
+    const mockupArray = Array.from(mockupRef.current);
+  }, []);
 
+  const mockupEnter = (e) => {
+    e.preventDefault();
 
-    useEffect(() => {
-        mockupRef.current = document.querySelectorAll(".mockup");
-        const mockupArray = Array.from(mockupRef.current);
-    }, [])
+    mockupRef.current.forEach((item) => {
+      item.classList.add("on");
+    });
+  };
 
-    const mockupEnter = (e) => {
-        e.preventDefault();
+  const mockupLeave = (e) => {
+    e.preventDefault();
+    mockupRef.current.forEach((item) => {
+      item.classList.remove("on");
+    });
+  };
 
-        mockupRef.current.forEach(item => {
-            item.classList.add("on");
-        });
-    }
+  const w3c = (work) => {
+    setIsClicked(true);
+    setWorkName(work);
+  };
 
-    const mockupLeave = (e) => {
+  return (
+    <li className={className} key={keyNum}>
+      <div className="left">
+        <div
+          className="mockup"
+          onMouseEnter={mockupEnter}
+          onMouseLeave={mockupLeave}
+          onTouchStart={mockupEnter}
+        >
+          <div className="video_box">
+            <video muted autoPlay loop playsInline>
+              {isIOS ? (
+                <source src={` ${process.env.PUBLIC_URL}${iosVideoUrl}`} />
+              ) : (
+                <source src={` ${process.env.PUBLIC_URL}${videoUrl}`} />
+              )}
+            </video>
+          </div>
+        </div>
 
-        e.preventDefault();
-        mockupRef.current.forEach(item => {
-            item.classList.remove("on");
-        });
+        <div className="w3c">
+          <ul>
+            <li>기여도 100%</li>
+            <li
+              onClick={() => {
+                w3c({ className });
+              }}
+            >
+              W3C 웹 접근성 검사 결과 보기
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="right">
+        <div className="project_tit">
+          <h2>{name}</h2>
+          <ul className="used_skills">
+            {skill.map((item, idx2) => (
+              <li key={idx2}>{item}</li>
+            ))}
+          </ul>
+        </div>
 
-    }
+        <dl className="technique_list">
+          <dt>주요 기술</dt>
+          {function1.map((item, idx1) => (
+            <dd key={idx1}>🤍&nbsp;&nbsp;{item}</dd>
+          ))}
+        </dl>
 
-    const w3c = (work) => {
-        setIsClicked(true);
-        setWorkName(work);
-    }
-
-
-    return (
-
-        <li className={className} key={keyNum}>
-            <div className="left">
-                <div className="mockup"
-                    onMouseEnter={mockupEnter}
-                    onMouseLeave={mockupLeave}
-                    onTouchStart={mockupEnter}
-                >
-                    <div className="video_box">
-
-                        <video muted autoPlay loop playsInline>
-                            {isIOS ?
-                                <source src={` ${process.env.PUBLIC_URL}${iosVideoUrl}`} />
-                                :
-                                <source src={` ${process.env.PUBLIC_URL}${videoUrl}`} />
-                            }
-
-                        </video>
-
-                    </div>
-                </div>
-
-                <div className="w3c">
-                    <ul>
-
-                        <li>기여도 100%</li>
-                        <li onClick={() => {
-                            w3c({ className })
-                        }}>W3C 웹 접근성 검사 결과 보기</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="right">
-                <div className="project_tit">
-                    <h2>{name}</h2>
-                    <ul className="used_skills">
-                        {skill.map((item, idx2) =>
-                            <li key={idx2}>{item}</li>
-                        )}
-                    </ul>
-                </div>
-
-
-
-                <dl className="technique_list">
-                    <dt>주요 기술</dt>
-                    {function1.map((item, idx1) =>
-                        <dd key={idx1}>🤍&nbsp;&nbsp;{item}</dd>
-                    )}
-                </dl>
-
-                <ul className="link">
-                    <li><a href={homepage} title="사이트로 이동" target="_blank"><FaHome /></a></li>
-                    <li><a href={github} title="깃헙 레파지토리로 이동" target="_blank"><FaGithub /></a></li>
-                </ul>
-
-            </div>
-
-        </li >
-
-    )
+        <ul className="link">
+          <li>
+            <a href={homepage} title="사이트로 이동" target="_blank">
+              <FaHome />
+            </a>
+          </li>
+          <li>
+            <a href={github} title="깃헙 레파지토리로 이동" target="_blank">
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </li>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,216 +1,212 @@
 import React, { useEffect, useRef, useState } from "react";
-import '../styles/work.scss';
+import "../styles/work.scss";
 import { FaHome, FaGithub, FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import project from '../data/project.json';
+import project from "../data/project.json";
 import Projects from "./Projects";
 import Accessibility from "./Accessibility";
 import { useNavigate } from "react-router-dom";
 
 function Work({ setCheckHome }) {
-    const [num, setNum] = useState(0);
-    const ulRef = useRef(null);
-    const liRef = useRef(null);
-    const prevRef = useRef(null);
-    const nextRef = useRef(null);
-    const navigate = useNavigate();
-    const [startY, setStartY] = useState(null);
-    const [isClicked, setIsClicked] = useState(false);
-    const [workName, setWorkName] = useState('');
+  const [num, setNum] = useState(0);
+  const ulRef = useRef(null);
+  const liRef = useRef(null);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  const navigate = useNavigate();
+  const [startY, setStartY] = useState(null);
+  const [isClicked, setIsClicked] = useState(false);
+  const [workName, setWorkName] = useState("");
 
+  let isScrolling = true;
+  setTimeout(() => {
+    isScrolling = false;
+  }, 300);
 
-    let isScrolling = true;
-    setTimeout(() => {
-        isScrolling = false;
-    }, 300);
+  const handleTouchStart = (e) => {
+    setStartY(e.touches[0].clientY);
+  };
 
-    const handleTouchStart = (e) => {
-        setStartY(e.touches[0].clientY);
-    };
+  const handleTouchEnd = () => {
+    setStartY(null);
+  };
 
-    const handleTouchEnd = () => {
-        setStartY(null);
-    };
+  const handleTouchMove = (e) => {
+    if (startY) {
+      const currentY = e.touches[0].clientY;
+      const deltaY = currentY - startY;
 
-    const handleTouchMove = (e) => {
-        if (startY) {
-            const currentY = e.touches[0].clientY;
-            const deltaY = currentY - startY;
-
-            if (deltaY > -50 && window.scrollY <= 0) {
-                navigate('/skill');
-            } else if (deltaY < -50) {
-                navigate('/contact');
-            }
-        }
-    };
-
-    const handleWheel = (e) => {
-
-        if (isScrolling === false) {
-            if (e.deltaY < 0 && window.scrollY <= 0) {
-                isScrolling = true;
-                navigate('/skill');
-            } else if (e.deltaY > 50 && (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                isScrolling = true;
-                navigate('/contact');
-            }
-        }
-
-
-
-    };
-
-
-    useEffect(() => {
-        setCheckHome(true);
-
-        liRef.current = document.querySelectorAll(".work_list>ul>li");
-        ulRef.current = document.querySelector(".work_list>ul");
-
-        liRef.current.forEach(item => {
-            if (item.classList.contains("movie") || item.classList.contains("messanger") || item.classList.contains("next_movie")) {
-                item.querySelector(".left").querySelector(".w3c").classList.add("noW3c");
-            }
-        }
-        )
-
-        prevRef.current = document.querySelector(".prev_btn");
-        nextRef.current = document.querySelector(".next_btn");
-
-        if (num === 0) {
-            prevRef.current.style.display = "none";
-        }
-
-
-    }, [])
-
-
-
-    const prevClick = () => {
-        setNum((prevNum) => {
-            let newNum = prevNum + 1;
-            if (newNum === 0) {
-                prevRef.current.style.display = "none";
-            } else {
-                prevRef.current.style.display = "block";
-                nextRef.current.style.display = "block";
-            }
-            if (newNum > 0) {
-                newNum = 0;
-                return newNum;
-            } else {
-                const ulWidth = ulRef.current.offsetWidth;
-                ulRef.current.style.opacity = 0;
-                setTimeout(() => {
-                    ulRef.current.style.left = newNum * (ulWidth / 6) + "px";
-                    setTimeout(() => {
-                        ulRef.current.style.opacity = 1;
-                    }, 100);
-                }, 200)
-                return newNum;
-            }
-
-        });
+      if (deltaY > -50 && window.scrollY <= 0) {
+        navigate("/skill");
+      } else if (deltaY < -50) {
+        navigate("/contact");
+      }
     }
+  };
 
-    const nextClick = () => {
-        prevRef.current.style.opacity = "block";
-        setNum((prevNum) => {
-            let newNum = prevNum - 1;
-
-            if (newNum === -5) {
-                nextRef.current.style.display = "none";
-            } else {
-                prevRef.current.style.display = "block";
-                nextRef.current.style.display = "block";
-            }
-
-            if (newNum < -5) {
-
-                newNum = -5;
-
-                return newNum;
-            } else {
-
-                const ulWidth = ulRef.current.offsetWidth;
-
-                ulRef.current.style.opacity = 0;
-                setTimeout(() => {
-                    ulRef.current.style.left = newNum * (ulWidth / 6) + "px";
-                    setTimeout(() => {
-                        ulRef.current.style.opacity = 1;
-                    }, 100);
-                }, 200)
-            }
-
-            return newNum;
-        });
-
+  const handleWheel = (e) => {
+    if (isScrolling === false) {
+      if (e.deltaY < 0 && window.scrollY <= 0) {
+        isScrolling = true;
+        navigate("/skill");
+      } else if (
+        e.deltaY > 50 &&
+        window.innerHeight + window.scrollY >= document.body.offsetHeight
+      ) {
+        isScrolling = true;
+        navigate("/contact");
+      }
     }
+  };
 
-    window.addEventListener("resize", () => {
-        if (ulRef.current) {
-            const ulWidth = ulRef.current.offsetWidth;
-            ulRef.current.style.left = num * (ulWidth / 6) + "px";
-        }
+  useEffect(() => {
+    setCheckHome(true);
+
+    liRef.current = document.querySelectorAll(".work_list>ul>li");
+    ulRef.current = document.querySelector(".work_list>ul");
+
+    liRef.current.forEach((item) => {
+      if (
+        item.classList.contains("movie") ||
+        item.classList.contains("messanger") ||
+        item.classList.contains("next_movie")
+      ) {
+        item
+          .querySelector(".left")
+          .querySelector(".w3c")
+          .classList.add("noW3c");
+      }
     });
 
-    return (<>
-        <div className="work"
-            onWheel={handleWheel}
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
-        >
-            {isClicked ?
-                <Accessibility workName={workName} setWorkName={setWorkName} setIsClicked={setIsClicked} />
-                :
-                <></>
-            }
-            <div className="work_inner">
-                <div className="work_tit">
-                    <h1 className="on">WORK</h1>
-                </div>
-                <div className="work_con">
+    prevRef.current = document.querySelector(".prev_btn");
+    nextRef.current = document.querySelector(".next_btn");
 
-                    <div className="work_list">
-                        <ul>
-                            {project.Project.map((project, idx) => (
-                                <Projects
-                                    key={idx}
-                                    name={project.name}
-                                    className={project.class}
-                                    skill={project.skill}
-                                    function1={project.function}
-                                    homepage={project.page_link}
-                                    github={project.github_link}
-                                    videoUrl={project.video_url}
-                                    iosVideoUrl={project.ios_video_url}
-                                    setIsClicked={setIsClicked}
-                                    setWorkName={setWorkName}
-                                />
-                            ))}
+    if (num === 0) {
+      prevRef.current.style.display = "none";
+    }
+  }, []);
 
+  const prevClick = () => {
+    setNum((prevNum) => {
+      let newNum = prevNum + 1;
+      if (newNum === 0) {
+        prevRef.current.style.display = "none";
+      } else {
+        prevRef.current.style.display = "block";
+        nextRef.current.style.display = "block";
+      }
+      if (newNum > 0) {
+        newNum = 0;
+        return newNum;
+      } else {
+        const ulWidth = ulRef.current.offsetWidth;
+        ulRef.current.style.opacity = 0;
+        setTimeout(() => {
+          ulRef.current.style.left = newNum * (ulWidth / 7) + "px";
+          setTimeout(() => {
+            ulRef.current.style.opacity = 1;
+          }, 100);
+        }, 200);
+        return newNum;
+      }
+    });
+  };
 
-                        </ul>
-                    </div>
+  const nextClick = () => {
+    prevRef.current.style.opacity = "block";
+    setNum((prevNum) => {
+      let newNum = prevNum - 1;
 
-                </div>
+      if (newNum === -6) {
+        nextRef.current.style.display = "none";
+      } else {
+        prevRef.current.style.display = "block";
+        nextRef.current.style.display = "block";
+      }
+
+      if (newNum < -6) {
+        newNum = -6;
+
+        return newNum;
+      } else {
+        const ulWidth = ulRef.current.offsetWidth;
+
+        ulRef.current.style.opacity = 0;
+        setTimeout(() => {
+          ulRef.current.style.left = newNum * (ulWidth / 7) + "px";
+          setTimeout(() => {
+            ulRef.current.style.opacity = 1;
+          }, 100);
+        }, 200);
+      }
+
+      return newNum;
+    });
+  };
+
+  window.addEventListener("resize", () => {
+    if (ulRef.current) {
+      const ulWidth = ulRef.current.offsetWidth;
+      ulRef.current.style.left = num * (ulWidth / 7) + "px";
+    }
+  });
+
+  return (
+    <>
+      <div
+        className="work"
+        onWheel={handleWheel}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {isClicked ? (
+          <Accessibility
+            workName={workName}
+            setWorkName={setWorkName}
+            setIsClicked={setIsClicked}
+          />
+        ) : (
+          <></>
+        )}
+        <div className="work_inner">
+          <div className="work_tit">
+            <h1 className="on">WORK</h1>
+          </div>
+          <div className="work_con">
+            <div className="work_list">
+              <ul>
+                {project.Project.map((project, idx) => (
+                  <Projects
+                    key={idx}
+                    name={project.name}
+                    className={project.class}
+                    skill={project.skill}
+                    function1={project.function}
+                    homepage={project.page_link}
+                    github={project.github_link}
+                    videoUrl={project.video_url}
+                    iosVideoUrl={project.ios_video_url}
+                    setIsClicked={setIsClicked}
+                    setWorkName={setWorkName}
+                  />
+                ))}
+              </ul>
             </div>
-        </div >
-
-        <div className="list_control">
-            <a className="prev_btn" onClick={prevClick}><FaArrowLeft /></a>
-
-            <a className="next_btn" onClick={nextClick}><FaArrowRight /></a>
+          </div>
         </div>
+      </div>
+
+      <div className="list_control">
+        <a className="prev_btn" onClick={prevClick}>
+          <FaArrowLeft />
+        </a>
+
+        <a className="next_btn" onClick={nextClick}>
+          <FaArrowRight />
+        </a>
+      </div>
     </>
-
-
-
-    )
-
+  );
 }
 
 export default Work;
-
